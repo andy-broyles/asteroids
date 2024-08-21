d7429ba82342d4e0ec66e3ff70effe02cf08828a
Andy-Broyles <andybroy@gmail.com>
2024-08-20 22:27:11 -0500

    look at what i did here

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pygame
-pygame.init()
-
 from constants import *
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         screen.fill("black")
         pygame.display.flip()
 
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
